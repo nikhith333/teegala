2c5ba10ab7958484b37ca599c082c8cb3ca23f91
nikhith333 <nikhithreddy2020@gmail.com>
2025-09-11 22:32:02 +0530

    Datatypes

---
+++ tnsif/src/com/tnsif/first/Datatypes.java
@@ -0,0 +1,24 @@
+package com.tnsif.first;
+public class Datatypes {
+	public static void main(String[] args) {		
+		byte byteVar = 10;
+        short shortVar = 100;
+        int intVar = 100000;
+        long longVar = 10000000000L;
+        float floatVar = 3.14f;
+        double doubleVar = 3.14159265359;
+        char charVar = 'A';
+        boolean boolVar = true;
+        String strVar = "Hello, Welcome to TNS!";
+        System.out.println("Byte Value: " + byteVar);
+        System.out.println("Short Value: " + shortVar);
+        System.out.println("Int Value: " + intVar);
+        System.out.println("Long Value: " + longVar);
+        System.out.println("Float Value: " + floatVar);
+        System.out.println("Double Value: " + doubleVar);
+        System.out.println("Char Value: " + charVar);
+        System.out.println("Boolean Value: " + boolVar);
+        System.out.println("String Value: " + strVar);
+	}
+
+}
